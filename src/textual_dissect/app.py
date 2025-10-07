7cb14a979e9e805925a24cb50416aca82f863c1e
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2025-10-07 02:40:37 +0100

    refactor: get widget base classes before child classes
    Rearrange the loops to get widget base classes before child classes to
    help with debugging.

--- src/textual_dissect/app.py
+++ src/textual_dissect/app.py
@@ -147,6 +147,18 @@ def get_widget_details(
         raw_default_css = class_.DEFAULT_CSS
         default_css = dedent(raw_default_css).strip()
 
+        base_classes: list[str] = []
+        current_class = class_
+        while True:
+            base_classes.append(current_class.__name__)
+            for base in current_class.__bases__:
+                if issubclass(base, DOMNode):
+                    current_class = base
+                    break
+            else:
+                break
+        base_classes.reverse()
+
         child_widgets: list[str] = []
         if widget_type != WidgetType.CONTAINER:
             for name, obj in inspect.getmembers(module, inspect.isclass):
@@ -162,18 +174,6 @@ def get_widget_details(
                         continue
                     else:
                         child_widgets.append(name)
-
-        base_classes: list[str] = []
-        while True:
-            base_classes.append(class_.__name__)
-            for base in class_.__bases__:
-                if issubclass(base, DOMNode):
-                    class_ = base
-                    break
-            else:
-                break
-
-        base_classes.reverse()
 
         _WIDGET_DETAILS_CACHE[widget_class] = WidgetDetails(
             docs_url=docs_url,
